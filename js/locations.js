875183a2acbc675343206570024dba27be60fe15
SalmaSal <bs-4@hotmail.com>
2017-11-04 02:14:17 +0300

    Add files via upload

---
+++ js/locations.js
@@ -0,0 +1,37 @@
+//===============Model=============//
+var locations = [{
+    title: 'King Abdullah Petroleum Studies and Research Center',
+    address: {
+        lat: 24.879240,
+        lng: 46.722068
+    },
+    description: 'international research center in Riyadh'
+}, {
+    title: 'Princess Nora bint Abdul Rahman University',
+    address: {
+        lat: 24.846461,
+        lng: 46.724731
+    },
+    description: 'first university for femel only'
+}, {
+    title: 'Kingdom Centre',
+    address: {
+        lat: 24.712045,
+        lng: 46.675962
+    },
+    description: 'shoping mall '
+}, {
+    title: 'The Ritz-Carlton Hotel Company',
+    address: {
+        lat: 24.666362,
+        lng: 46.630396
+    },
+    description: "Hotel"
+}, {
+    title: 'King Abdullah Financial District',
+    address: {
+        lat: 24.761956,
+        lng: 46.640434
+    },
+    description: 'King Abdullah Financial District'
+}];
